cd6756e6b8e8fa5762c3c9c63f1e5df3ef6a7d88
AsrarOthman <asrarothman@gmail.com>
2024-11-26 21:59:03 +0800

    Updates

---
+++ precache-manifest.02fc9e4efa9e99e95a7d1895777bd561.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a354a00438ba2d6deb2a774c2a262c52",
+    "url": "/jpnp/index.html"
+  },
+  {
+    "revision": "9bde81edbb52db4f3f33",
+    "url": "/jpnp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8c9061cc451db7ce1867",
+    "url": "/jpnp/static/js/2.8de64fce.chunk.js"
+  },
+  {
+    "revision": "2c10491364e9bdf48063",
+    "url": "/jpnp/static/js/3.fc5cbf83.chunk.js"
+  },
+  {
+    "revision": "9bde81edbb52db4f3f33",
+    "url": "/jpnp/static/js/main.430e2963.chunk.js"
+  },
+  {
+    "revision": "172596d43d61f37c7c05",
+    "url": "/jpnp/static/js/runtime~main.6db6abd6.js"
+  }
+]);
